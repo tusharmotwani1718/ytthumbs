ba1f075863466585d2f33abd9eba13a63301eaee
Tushar Motwani <tusharmotwani89@gmail.com>
2025-08-31 18:39:46 +0530

    add .envsample.js

---
+++ backend/.envsample.js
@@ -0,0 +1,12 @@
+GEMINI_API_KEY="<your_gemini_api_key>"
+OPENAI_API_KEY="<your_openai_api_key>"
+
+
+CLOUDINARY_CLOUD_NAME="<your_cloudinary_cloud_name>"
+CLOUDINARY_API_KEY="<your_cloudinary_api_key>"
+CLOUDINARY_API_SECRET="<your_cloudinary_api_secret>"
+
+MONGODB_URI="<your_mongodb_uri>"
+PORT="<your_port>"
+// # dev:
+CORS_ORIGIN="<your_dev_cors_origin>"
